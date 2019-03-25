b494e6826a6aa5cb3707160d1b6f83e618f6b129
NadeemAhmad007 <44241167+NadeemAhmad007@users.noreply.github.com>
2019-03-25 23:51:01 +0100

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,8 +1,41 @@
-public class Palindrome{
+
 
 	//todo: Create a method isPalindrome which takes a String as input and returns true if it is a palindrome and false otherwise.
 	//A palindrome is a word which can be read backwards and forwards in the same way., e.g. 'hannah'.
 	//Ignore upper and lower case. 'Hannah' is a palindrome, too.
 	//Ignore also spaces. 'Taco cat' is a valid palindrome.
 
+import java.util.Scanner;
+
+public class isPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ss = new Scanner(System.in);
+	    System.out.print("Enter the your String : ");
+	    // Below Statement used for getting String including sentence
+	    String s = ss.nextLine(); 
+	   char[] w = s.toCharArray();
+	    s= s.replaceAll("\\s+", "");
+	    s = s.toLowerCase();
+	   // Below Statement used for return the first word in the sentence
+	     //System.out.println(s);
+	   boolean r = isPalindrom(s);
+	   System.out.println(r);
+	  
+	 
+		}
+	public static boolean isPalindrom(String s){
+	    int i1 = 0;
+	    int i2 = s.length() - 1;
+	    while (i2 > i1) {
+	        if (s.charAt(i1) != s.charAt(i2)) {
+	            return false;
+	        }
+	        ++i1;
+	        --i2;
+	    }
+	    return true;
+	}
+	
 }
